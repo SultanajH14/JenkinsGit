69b7874b6e3925c190b2680aa179184ef207e485
unknown <sultana.jarfin@gmail.com>
2022-04-02 23:14:09 -0400

    added anothermethod

--- Mavenjenkinprac/src/test/java/com/bit/test/BaseTest.java
+++ Mavenjenkinprac/src/test/java/com/bit/test/BaseTest.java
@@ -1,8 +1,15 @@
 package com.bit.test;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-	public void openBrowser() {
-	
+public class BaseTest {
+public WebDriver dr;
+	public void openBrowser(String browser, String url) {
+		if (browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\SWEETY\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			dr = new ChromeDriver();
+			dr.get(url);
+		}
 }
 }

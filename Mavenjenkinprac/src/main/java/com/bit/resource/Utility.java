e2da8be8f0a168f0852b6ce4604ee9e104a1a9cf
unknown <sultana.jarfin@gmail.com>
2022-04-03 11:45:12 -0400

    added method

--- Mavenjenkinprac/src/main/java/com/bit/resource/Utility.java
+++ Mavenjenkinprac/src/main/java/com/bit/resource/Utility.java
@@ -2,10 +2,17 @@ package com.bit.resource;
 
 import org.openqa.selenium.WebDriver;
 
+
+
 public class Utility {
 	WebDriver dr;
 	public void geturl() {
 		dr.get("https://www.facebook.com/");
-	}
+	
 
 }
+	public void sp() {
+		
+	}
+}
+	
